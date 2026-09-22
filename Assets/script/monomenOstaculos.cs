3cca517824cf557fad4dde71e62f44fd79a6b71f
danielviancha06-cloud <danielviancha06@gmail.com>
2026-09-22 17:53:37 -0500

    actualizacion Martes
    nivel 5

---
+++ Assets/script/monomenOstaculos.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class monomenOstaculos : MonoBehaviour
+{
+    [SerializeField] private float spped;
+
+    [SerializeField] private GameObject targed;
+    private void Update()
+    {
+
+        //transform.Translate(Vector3.right * spped * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, targed.transform.position, spped * Time.deltaTime);
+        transform.Rotate(Vector3.forward * spped * Time.deltaTime);
+
+        //transform.Rotate(Quaternion.RotateTowards(transform.rotation, targed.transform.rotation, spped * Time.deltaTime);
+
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, targed.transform.rotation, spped * Time.deltaTime);
+
+    }
+
+
+
+
+}
+
+
